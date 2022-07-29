94622ce0f512ac4ed962eeaec76ed32f2320cb25
Cedric Spindler <cedric.spindler@gmail.com>
2022-07-29 19:39:34 +0200

    Set api URL from environment

--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -1,15 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { environment } from 'src/environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
+  private apiUrl = environment.apiUrl;
+
   constructor(private http: HttpClient) { }
 
   getProgress() {
-    return this.http.get('http://127.0.0.1:8000/progress/');
+    return this.http.get(`${this.apiUrl}/queue/progress/`);
+  }
+
+  getInput() {
+    return this.http.get(`${this.apiUrl}/queue/input/`);
   }
 
 }
