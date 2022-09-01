023517bdd2eb838d74c255df75f3b9e62462e6f0
Cedric Spindler <cedric.spindler@gmail.com>
2022-09-01 22:22:01 +0200

    Integrate auth service in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from './shared/authentication.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,22 @@ export class AppComponent {
 
   title = 'Mitwelten ML Dashboard';
 
+  loggedIn: boolean | undefined;
+
+  constructor(private authService: AuthenticationService) {
+
+  }
+
+  ngOnInit(): void {
+    this.checkLogin();
+    this.authService.authState.subscribe(authState => this.loggedIn = authState);
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+
+  private checkLogin() {
+    this.authService.checkLogin().subscribe(state => this.loggedIn = state)
+  }
 }
