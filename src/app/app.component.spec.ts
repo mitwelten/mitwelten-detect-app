1a8f5e0392e1b1dc25c78cfca9e7d67e27e25c48
Cedric Spindler <cedric.spindler@gmail.com>
2022-09-04 20:17:45 +0200

    Consistent naming

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'queue-manager'`, () => {
+  it(`should have as title 'detect-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('queue-manager');
+    expect(app.title).toEqual('detect-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('queue-manager app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('detect-app app is running!');
   });
 });
